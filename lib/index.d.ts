a3a3386b8b683139e4a9a79cd992d4462af357bc
xiaoHan <qiaoming@zcool.com.cn>
2018-11-16 17:07:11 +0800

    fix

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,4 +2,3 @@ export { default as Input } from './input';
 export { default as Dialog } from './dialog';
 export { default as Button } from './button';
 export { default as InputArea } from './input/input-area';
-export { default as Icon } from './icon';
